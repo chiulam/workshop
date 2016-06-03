b33454a947b8f30d3cace359522154c756cf6c69
Chiu Lam <happylam23@gmail.com>
2016-06-03 18:21:21 +0800

    Finish 05

--- 05-error-handling/index.js
+++ 05-error-handling/index.js
@@ -4,13 +4,16 @@ var koa = require('koa');
 var app = module.exports = koa();
 
 app.use(function* errorHandler(next) {
-  try {
-    yield next;
-  } catch (err) {
-    // your error handling logic goes here
-  }
+	try {
+		yield next;
+	} catch (err) {
+		// your error handling logic goes here
+		this.response.status = 500;
+		this.response.body = 'internal server error';
+		app.emit('error', err, this);
+	}
 });
 
 app.use(function* () {
-  throw new Error('boom!');
+	throw new Error('boom!');
 });
