3aac8706df9ad49a07a95da5fed36e5fbe7ea2f3
Chiu Lam <happylam23@gmail.com>
2016-06-06 10:25:12 +0800

    Finish 06

--- 06-content-negotiation/index.js
+++ 06-content-negotiation/index.js
@@ -2,18 +2,26 @@
 var koa = require('koa');
 
 /**
- * This is a promisified version of zlib's gzip function,
- * allowing you to simply `yield` it without "thunkifying" it.
- *
- *   app.use(function* (next) {
- *     this.response.set('Content-Encoding', 'gzip');
- *     this.response.body = yield gzip('something');
- *   })
- */
+* This is a promisified version of zlib's gzip function,
+* allowing you to simply `yield` it without "thunkifying" it.
+*
+*   app.use(function* (next) {
+*     this.response.set('Content-Encoding', 'gzip');
+*     this.response.body = yield gzip('something');
+*   })
+*/
 var gzip = require('mz/zlib').gzip;
 
 var app = module.exports = koa();
 
 app.use(function* () {
 
-})
+	var acceptsEncodings = this.request.acceptsEncodings('gzip', 'identity');
+	if (acceptsEncodings === 'gzip') {
+		this.response.set('Content-Encoding', 'gzip');
+		this.response.body = yield gzip('hello world');
+	} else {
+		this.response.set('Content-Encoding', 'identity');
+		this.response.body = 'hello world';
+	}
+});
